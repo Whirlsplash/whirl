761add819857aa6da7b5c1688f1abda437db5a0d
Fuwn <contact@fuwn.me>
2021-03-21 19:29:50 +0000

    chore: Create sqlx migrations

---
+++ migrations/20210322015725_serial_numbers.sql
@@ -0,0 +1,5 @@
+CREATE TABLE "serial_numbers" (
+	"serial_number"	TEXT NOT NULL UNIQUE,
+	"user_name"	TEXT NOT NULL,
+	"serial_status"	TEXT NOT NULL
+);
---
+++ migrations/20210322015737_user_properties.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "user_propertyies" (
+	"user_name"	TEXT NOT NULL,
+	"property_id"	INTEGER NOT NULL,
+	"property_flags" INTEGER NOT NULL,
+	"property_access"	INTEGER NOT NULL,
+	"property_string_value"	TEXT,
+	"property_binary_value"	TEXT
+);
