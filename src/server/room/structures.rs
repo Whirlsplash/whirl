3d0e29c48547c276b8bce07dff10c560c0949b18
Fuwn <contact@fuwn.me>
2021-03-23 16:48:37 +0000

    etc: Remove unused module and implement structures
    As you might see, I've began expanding on the RoomServer. Hopefully within the next week, you might be able to see connected users' avatars.

---
+++ src/server/room/structures.rs
@@ -0,0 +1,15 @@
+// Unused struct, purely for reference.
+// struct RoomSubscribeInfo {
+// 	pub x: f64,
+// 	pub y: f64,
+// 	pub z: f64,
+// 	pub d: f64,
+// }
+
+pub struct Room {
+	distance: i16,
+	x: i16,
+	y: i16,
+	z: i16,
+	room_number: i16,
+}
