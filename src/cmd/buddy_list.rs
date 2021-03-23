be21fa8351822d6235b903bd16ab74b887668958
Fuwn <fuwnzy@gmail.com>
2021-03-23 13:34:23 -0700

    fix: Buddy name length not being properly set
    Finally...

--- src/cmd/buddy_list.rs
+++ src/cmd/buddy_list.rs
@@ -4,8 +4,7 @@ pub fn create_buddy_list_notify_command(buddy: &str) -> Vec<u8> {
 	let mut buddy_list_notify = Vec::with_capacity(5 + buddy.len());
 	buddy_list_notify.push(0x01); // ?
 	buddy_list_notify.push(0x1E); // BUDDYLISTNOTIFY
-	// The number of buddies you are being notified of?
-	buddy_list_notify.push(0x0A); // 01 // ^
+	buddy_list_notify.push(buddy.len() as u8); // Buddy name length
 	for i in buddy.bytes() { buddy_list_notify.push(i); } // Buddy name
 	buddy_list_notify.push(0x01); // Is buddy logged on?
 	// Insert data length as first byte.
