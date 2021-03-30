d651bd7755ca10bcd9c7d6c1a3c885355d52ea9e
Fuwn <contact@fuwn.me>
2021-03-29 23:18:48 -0700

    format: Preinitialize vector with elements

--- src/server/auto/cmd/room.rs
+++ src/server/auto/cmd/room.rs
@@ -1,8 +1,10 @@
 pub fn create_room_id_redirect_command(room_name: &str, room_id: usize) -> Vec<u8> {
-	let mut room_id_redirect = Vec::new();
+	let mut room_id_redirect = vec![
+		0x01, // ?
+		0x1A, // Command type
+	];
+
 	// room_id_redirect.push(room_id_redirect.len() as u8 + 1); // Data length
-	room_id_redirect.push(0x01); // ?
-	room_id_redirect.push(0x1A); // Command type1
 	room_id_redirect.push(room_name.len() as u8); // UTF/ room name length
 	for i in room_name.bytes() { room_id_redirect.push(i); } // Push `room_name`
 	// for i in "<dimension-1>".bytes() { room_id_redirect.push(i); } // Push room number
