fc86d754351e9390bd609a756cb2075a4cd5dcba
Fuwn <contact@fuwn.me>
2021-04-24 18:25:38 +0000

    chore: Add Nix and Docker support

---
+++ default.nix
@@ -0,0 +1,20 @@
+ { system ? builtins.currentSystem }:
+
+ let
+    sources = import ./nix/sources.nix;
+    pkgs = import sources.nixpkgs { };
+    whirl = import ./whirl.nix { inherit sources pkgs; };
+
+    name = "Whirlsplash/whirl";
+    tag = "latest";
+
+in pkgs.dockerTools.buildLayeredImage {
+    inherit name tag;
+    contents = [ whirl ];
+
+    config = {
+        Cmd = [ "/bin/whirl" ];
+        Env = [ ];
+        WorkingDir = "/";
+    };
+}
