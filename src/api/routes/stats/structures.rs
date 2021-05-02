6f839a24e989353571c31870cae91c6aeabcb75b
Fuwn <contact@fuwn.me>
2021-05-02 17:20:29 +0000

    feat(api): flush out statistics endpoint

--- src/api/routes/stats/structures.rs
+++ src/api/routes/stats/structures.rs
@@ -5,5 +5,19 @@ use serde_derive::Serialize;
 
 #[derive(Serialize)]
 pub struct Statistics {
-  pub message: String,
+  pub system:  StatisticsSystem,
+  pub process: StatisticsProcess,
+}
+#[derive(Serialize)]
+pub struct StatisticsSystem {
+  #[serde(rename = "type")]
+  pub os_type: String,
+  pub release: String,
+  pub uptime:  String,
+}
+#[derive(Serialize)]
+pub struct StatisticsProcess {
+  pub memory_usage: String,
+  pub cpu_usage:    String,
+  // pub uptime:       String,
 }
