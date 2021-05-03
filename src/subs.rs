82fbdc9651acfce184e2fdb34dc35fdd2e80746e
Fuwn <contact@fuwn.me>
2021-05-03 11:13:32 -0700

    refactor(global): remove all the fluff from main file

---
+++ src/subs.rs
@@ -0,0 +1,44 @@
+// Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
+// SPDX-License-Identifier: GPL-3.0-only
+
+use std::error::Error;
+
+use crate::{
+  api::API,
+  config::Config,
+  server::{
+    distributor::Distributor,
+    hub::Hub,
+    server::{
+      Server,
+      ServerType::{AutoServer, RoomServer},
+    },
+  },
+};
+
+pub async fn run() -> Result<(), Box<dyn Error>> {
+  let threads = vec![
+    tokio::spawn(async move {
+      let _ = Distributor::listen(
+        &*format!("0.0.0.0:{}", Config::get().unwrap().distributor.port),
+        AutoServer,
+      )
+      .await;
+    }),
+    tokio::spawn(async move {
+      let _ = Hub::listen(
+        &*format!("0.0.0.0:{}", Config::get().unwrap().hub.port),
+        RoomServer,
+      )
+      .await;
+    }),
+    tokio::spawn(async move {
+      let _ = API::listen();
+    }),
+  ];
+  for thread in threads {
+    let _ = thread.await;
+  }
+
+  Ok(())
+}
