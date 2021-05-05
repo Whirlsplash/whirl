b4e826012344736403f2e4ea44c1a3ff881a939f
Fuwn <contact@fuwn.me>
2021-05-05 18:20:34 +0000

    build(make): create watchable make task

--- src/subs.rs
+++ src/subs.rs
@@ -36,6 +36,13 @@ pub async fn run() -> ! {
     }),
   ];
 
-  std::thread::sleep(std::time::Duration::from_secs(2));
-  Prompt::handle();
+  if std::env::var("DISABLE_PROMPT").unwrap_or("false".to_string()) == "true" {
+    info!("starting with prompt disabled");
+    loop {
+      std::thread::sleep(std::time::Duration::default());
+    }
+  } else {
+    std::thread::sleep(std::time::Duration::from_secs(2));
+    Prompt::handle();
+  }
 }
