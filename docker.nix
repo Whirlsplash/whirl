2e08c169cd350c73bc197f918b36dd10f6eb866d
Fuwn <fuwnzy@gmail.com>
2021-05-06 13:00:32 -0700

    docs(docker): sha256 notes

--- docker.nix
+++ docker.nix
@@ -15,6 +15,12 @@ let
         imageName = "alpine";
         imageDigest = "sha256:def822f9851ca422481ec6fee59a9966f12b351c62ccb9aca841526ffaa9f748";
         # https://nixos.wiki/wiki/Docker
+        #
+        # The above article didn't even work for me, ROFL.
+        # `nix-build docker.nix` threw an error about the sha256 being
+        # incorrect, but it also spat our the expected sha256...
+        #
+        # so I just replaced it...
         sha256 = "1z6fh6ry14m5cpcjfg88vn2m36garmgdagr4vfc3pm1z3kph639n";
         finalImageTag = "alpine";
         finalImageName = "3.13.5";
