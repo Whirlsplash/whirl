9a9af1817d706a98dcc9481f3f4d63d7f8e2f3a9
Fuwn <fuwnzy@gmail.com>
2021-05-17 15:12:30 -0700

    fix(global): add features, warnings, etc. to the rest of the crates

--- whirl_config/src/lib.rs
+++ whirl_config/src/lib.rs
@@ -1,6 +1,16 @@
 // Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
 // SPDX-License-Identifier: GPL-3.0-only
 
+#![feature(
+  type_ascription,
+  hash_set_entry,
+  type_name_of_val,
+  decl_macro,
+  proc_macro_hygiene
+)]
+#![warn(rust_2018_idioms)]
+#![recursion_limit = "128"]
+
 #[macro_use]
 extern crate serde_derive;
 #[macro_use]
