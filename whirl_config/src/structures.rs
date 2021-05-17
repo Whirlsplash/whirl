82257d46ed0dfeefab1c1b3617a1ed1801808c84
Fuwn <contact@fuwn.me>
2021-05-17 11:06:36 +0000

    refactor(global): move constants and structures to their respective modules

---
+++ whirl_config/src/structures.rs
@@ -0,0 +1,37 @@
+// Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
+// SPDX-License-Identifier: GPL-3.0-only
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WhirlsplashConfig {
+  pub worldsmaster_username: String,
+  pub ip:                    String,
+  pub api:                   WhirlsplashApiConfig,
+  pub prompt:                WhirlsplashPromptConfig,
+  pub log:                   WhirlsplashLogConfig,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WhirlsplashApiConfig {
+  pub port: i64,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WhirlsplashPromptConfig {
+  pub enable: bool,
+  pub ps1:    String,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct WhirlsplashLogConfig {
+  pub enable:     bool,
+  pub level:      i64,
+  pub everything: bool,
+  pub test:       bool,
+  pub file:       bool,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DistributorConfig {
+  pub worldsmaster_greeting: String,
+  pub port:                  i64,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct HubConfig {
+  pub port: i64,
+}
