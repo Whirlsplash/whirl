60eb4496a6849914b043c056427143b7434e8e15
Fuwn <fuwnzy@gmail.com>
2021-05-20 11:42:19 -0700

    refactor(whirl): move all of the main file's doings to a new file
    It looks better, I don't ever like main file polution.

---
+++ whirl/src/whirl.rs
@@ -0,0 +1,36 @@
+// Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
+// SPDX-License-Identifier: GPL-3.0-only
+
+use std::error::Error;
+
+use crate::{cli::Cli, utils::log::calculate_log_level};
+
+pub struct Whirl;
+impl Whirl {
+  pub async fn splash() -> Result<(), Box<dyn Error>> {
+    // Environment and CLI
+    let matches = Cli::setup();
+
+    // Logging
+    dotenv::dotenv().ok();
+    human_panic::setup_panic!();
+    let logger = flexi_logger::Logger::with_str(calculate_log_level());
+    if std::env::var("LOG_FILE").unwrap_or_else(|_| "true".to_string()) == "false"
+      || !whirl_config::Config::get().whirlsplash.log.file
+      || std::env::args().collect::<Vec<_>>()[1] == "clean"
+    // Cheeky as all hell.
+    {
+      logger.start()?;
+    } else {
+      logger
+        .print_message()
+        .log_to_file()
+        .directory("log")
+        .start()?;
+    }
+
+    Cli::execute(matches).await.unwrap();
+
+    Ok(())
+  }
+}
