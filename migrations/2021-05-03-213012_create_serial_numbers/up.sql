7e16a9efbfcf1d0fed108f80ab57b3631610d7e9
Fuwn <contact@fuwn.me>
2021-05-20 14:37:59 +0000

    style(migrations): fix sql linting errors

--- migrations/2021-05-03-213012_create_serial_numbers/up.sql
+++ migrations/2021-05-03-213012_create_serial_numbers/up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE serial_numbers (
-  serial_number TEXT NOT NULL UNIQUE,
-  user_name TEXT NOT NULL PRIMARY KEY ,
-  serial_status INTEGER NOT NULL
+    serial_number TEXT NOT NULL UNIQUE,
+    user_name TEXT NOT NULL PRIMARY KEY,
+    serial_status INTEGER NOT NULL
 )
--- migrations/2021-05-03-213037_create_user_properties/up.sql
+++ migrations/2021-05-03-213037_create_user_properties/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE user_properties (
-  user_name TEXT NOT NULL PRIMARY KEY,
-  property_id INTEGER NOT NULL,
-  property_flags INTEGER NOT NULL,
-  property_access INTEGER NOT NULL,
-  property_string_value INTEGER NOT NULL,
-  property_binary_value TEXT
+    user_name TEXT NOT NULL PRIMARY KEY,
+    property_id INTEGER NOT NULL,
+    property_flags INTEGER NOT NULL,
+    property_access INTEGER NOT NULL,
+    property_string_value INTEGER NOT NULL,
+    property_binary_value TEXT
 )
