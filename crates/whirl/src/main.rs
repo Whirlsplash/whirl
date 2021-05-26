ae7b3f07c4b22a0e523a195c9e3e430172a5ff03
Fuwn <contact@fuwn.me>
2021-05-26 22:22:50 +0000

    refactor(whirl): move whirl struct to lib

--- crates/whirl/src/main.rs
+++ crates/whirl/src/main.rs
@@ -2,4 +2,4 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> { whirl::whirl::Whirl::splash().await }
+async fn main() -> Result<(), Box<dyn std::error::Error>> { whirl::Whirl::splash().await }
