e9f005c4f82f67419609441224d3f1ec8ec6880e
Fuwn <fuwnzy@gmail.com>
2021-05-26 22:13:40 -0700

    style(whirl): don't use single used trait

--- crates/whirl/src/main.rs
+++ crates/whirl/src/main.rs
@@ -1,9 +1,7 @@
 // Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::error::Error;
-
 use whirl::whirl::Whirl;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> { Whirl::splash().await }
+async fn main() -> Result<(), Box<dyn std::error::Error>> { Whirl::splash().await }
