95fb0f956fd5cfacec3f7c69d6a10fcec3c95236
Fuwn <contact@fuwn.me>
2021-06-02 20:18:12 +0000

    refactor(server): use enum for command types

--- crates/whirl_server/src/cmd/constants.rs
+++ crates/whirl_server/src/cmd/constants.rs
@@ -3,6 +3,39 @@
 
 #![allow(dead_code)]
 
+#[derive(num_derive::FromPrimitive)]
+pub enum Command {
+  LongLoc  = 1,
+  State,
+  Prop,
+  ShortLoc,
+  RoomChng,
+  SessInit,
+  SessExit,
+  AppInit,
+  PropReq  = 10,
+  Disappr,
+  ApprActr,
+  RegObjId,
+  Text,
+  PropSet,
+  PropUpd,
+  Whisper,
+  Teleport,
+  RoomIdRq = 20,
+  RoomId,
+  Subscrib,
+  Unsubscr,
+  SubDist,
+  Redirect,
+  RedirId,
+  FingReq,
+  FingRep,
+  BuddyListUpdate,
+  BuddyListNotify,
+  Channel,
+}
+
 pub const LONGLOC: i32 = 1;
 pub const STATE: i32 = 2;
 pub const PROP: i32 = 3;
