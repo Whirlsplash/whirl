c61161b573dbe66a8e6c4becc9e4af802e65ce80
Fuwn <contact@fuwn.me>
2021-06-07 02:46:14 +0000

    test(whirl_common): create benchmarks

---
+++ benches/whirl_common.rs
@@ -0,0 +1,15 @@
+// Copyleft (ɔ) 2021-2021 The Whirlsplash Collective
+// SPDX-License-Identifier: GPL-3.0-only
+
+fn iai_benchmark_sort_vec_alphabetically() {
+  whirl_common::sort::vec_alphabetically(&mut vec!["a", "c", "d", "b"])
+}
+
+fn iai_benchmark_system_seconds_to_hrtime() -> String {
+  whirl_common::system::seconds_to_hrtime(1623058677)
+}
+
+iai::main!(
+  iai_benchmark_sort_vec_alphabetically,
+  iai_benchmark_system_seconds_to_hrtime
+);
