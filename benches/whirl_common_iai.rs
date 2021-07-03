03d1dc6012d346ba31f642e96429b996a3b30640
Fuwn <fuwnzy@gmail.com>
2021-07-03 13:15:37 -0700

    refactor(whirl_common): rename `seconds_to_hrtime` to `unixts_to_hrtime`

--- benches/whirl_common_iai.rs
+++ benches/whirl_common_iai.rs
@@ -5,11 +5,11 @@ fn iai_benchmark_sort_vec_alphabetically() {
   whirl_common::sort::vec_alphabetically(&mut vec!["a", "c", "d", "b"])
 }
 
-fn iai_benchmark_system_seconds_to_hrtime() -> String {
-  whirl_common::system::seconds_to_hrtime(1623058677)
+fn iai_benchmark_system_unixts_to_hrtime() -> String {
+  whirl_common::system::unixts_to_hrtime(1623058677)
 }
 
 iai::main!(
   iai_benchmark_sort_vec_alphabetically,
-  iai_benchmark_system_seconds_to_hrtime
+  iai_benchmark_system_unixts_to_hrtime
 );
