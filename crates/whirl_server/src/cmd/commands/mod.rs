ac7b9f963578ddd2955e1198fed6d07f88fd5939
Fuwn <contact@fuwn.me>
2021-07-04 04:02:23 +0000

    feat(whirl_server): create REGOBJID and APPRACTR commands

--- crates/whirl_server/src/cmd/commands/mod.rs
+++ crates/whirl_server/src/cmd/commands/mod.rs
@@ -2,9 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 pub mod action;
+pub mod appear_actor;
 pub mod buddy_list;
 pub mod property;
 pub mod redirect_id;
+pub mod register_object_id;
 pub mod room_id_request;
 pub mod session_exit;
 pub mod subscribe_distance;
